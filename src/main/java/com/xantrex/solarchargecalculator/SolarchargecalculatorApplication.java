f95d817cfb95a35a55c4ae95377476a7eb2889a0
Uday Syal <udaysyal16@gmail.com>
2026-02-12 12:13:22 -0800

    Added SpringBoot Project

---
+++ src/main/java/com/xantrex/solarchargecalculator/SolarchargecalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.xantrex.solarchargecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarchargecalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolarchargecalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xantrex/solarchargecalculator/SolarchargecalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xantrex.solarchargecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarchargecalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
