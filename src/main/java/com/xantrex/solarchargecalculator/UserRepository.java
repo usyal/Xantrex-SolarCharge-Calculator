36aac7640f643cd1a61c906eea34d1b7f710802b
Parsa <parsapoursaeid@icloud.com>
2026-02-26 14:13:03 -0800

    Remove duplicate root UserRepository

--- src/main/java/com/xantrex/solarchargecalculator/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.xantrex.solarchargecalculator;
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByName(String name);
-}
