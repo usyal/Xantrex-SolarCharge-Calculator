8503d50427489701fdb56e4b9b7d176d7f7d259f
Parsa <parsapoursaeid@icloud.com>
2026-02-26 19:59:50 -0800

    Add User model entity

---
+++ src/main/java/com/xantrex/solarchargecalculator/models/User.java
@@ -0,0 +1,45 @@
+package com.xantrex.solarchargecalculator.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false)
+    private String name; 
+    @Column(nullable = false)
+    private String password;
+
+    public User() {
+    }
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
